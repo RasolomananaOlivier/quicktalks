94f063512ee2acc69901638f225b74de7a168e88
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-06-02 15:15:58 +0300

    add landing page navbar

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import { Container, styled } from "@mui/material";
+import { Link as L } from "react-router-dom";
+
+const Nav = styled("nav")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: theme.spacing(2),
+}));
+
+const Link = styled(L)(({ theme }) => ({
+  color: theme.palette.grey[700],
+  textDecoration: "none",
+  fontWeight: "bold",
+  fontSize: "1.2rem",
+}));
 
 const LandingPage = () => {
   return (
-    <div>
-      I am the landing page.
-    </div>
+    <Container>
+      <Nav>
+        <div>
+          <img src="/Logo.svg" draggable="false" />
+        </div>
+        <Link to="/about">About</Link>
+      </Nav>
+    </Container>
   );
 };
 
